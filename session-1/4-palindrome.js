3a7dd61a9018b96c4e616a43902d268816c3d62b
jgzhernandez <jettlaagg@gmail.com>
2024-07-24 11:16:04 +0800

    Add functioning palindrome

--- session-1/4-palindrome.js
+++ session-1/4-palindrome.js
@@ -1,9 +1,16 @@
 function isPalindrome(word) {
-    // ... some logic here
+    return word === word.split('').reverse().join('')
   }
   
   const word = 'racecar'
   if (isPalindrome(word)) {
+    console.log('It is a palindrome!')
+  } else {
+    console.log('It is not a palindrome!')
+  }
+
+  const word2 = 'testnonpalindrome'
+  if (isPalindrome(word2)) {
     console.log('It is a palindrome!')
   } else {
     console.log('It is not a palindrome!')
