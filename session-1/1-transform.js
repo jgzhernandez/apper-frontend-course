14fa563923f84baf392919a6962d50454e932404
jgzhernandez <jettlaagg@gmail.com>
2024-07-24 11:36:43 +0800

    Add input validation to transform function

--- session-1/1-transform.js
+++ session-1/1-transform.js
@@ -1,7 +1,13 @@
 function transform(nums) {
-    return nums.map(num => num * num).sort((a, b) => a - b)
+    if(Array.isArray(nums) === true && nums.every(num => typeof num === "number")){
+      return nums.map(num => num * num).sort((a, b) => a - b)
+    }
+    return "Input an array of numbers."
   }
   
   const nums = [4,9,5,3,8]
   const sortedSquaredNums = transform(nums)
   console.log(sortedSquaredNums) // [9,16,25,64,81]
+  console.log(transform('hello'))
+  console.log(transform('hello'.split('')))
+  console.log(transform(1))
