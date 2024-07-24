1c673f31578a0d34243c844ee458945da69d37a8
jgzhernandez <jettlaagg@gmail.com>
2024-07-24 11:13:19 +0800

    Add functioning reverse

--- session-1/3-reverse.js
+++ session-1/3-reverse.js
@@ -1,7 +1,12 @@
 function reverse(word) {
-    // ... some logic here
+    if (typeof(word) === 'string'){
+        return word.split('').reverse().join('')
    }
+   return "Input a string."
+}
    
    const word = 'hello'
    const reversedWord = reverse(word)
    console.log(reversedWord) // 'olleh'
+   console.log(reverse(123)) // 'Input a string.'
+   console.log(reverse("race")) // 'race'
