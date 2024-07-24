433d797ee639ca9c8afcb809985e7ed492222cdb
jgzhernandez <jettlaagg@gmail.com>
2024-07-24 11:20:57 +0800

    Add functioning convertToVowels function

--- session-1/5-vowels.js
+++ session-1/5-vowels.js
@@ -1,5 +1,11 @@
 function convertToVowels(word) {
-    // ... some logic here
+    if (typeof(word) !== 'string') {
+        return 'Input a string.'
+    }
+    return word.split('').map((letter) => {
+        if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+            return letter
+        }}).join('')
   }
   
   const word = 'thisissomeword'
